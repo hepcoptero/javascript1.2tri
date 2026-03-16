0af61a38625341444be4a1337066caa9967572fa
Gustavo <gustavo.ferraz.coelho@escola.pr.gov.br>
2026-03-16 12:11:13 -0300

    update

--- 130322026.js
+++ 130322026.js
@@ -1,9 +1,16 @@
 let lucro = 30;
 let lucro2 = 10;
- function soma(a,b){
-    soma = (a + b)/2
- }
- soma(lucro,lucro2)
- function resultadodivisaosocio(lucro,lucro2){
-    return soma(lucro,lucro2)/2
- }
+let ari  
+function calculadora(ari,a,b){
+   switch(ari){
+      case '+':
+         resultado = a + b;
+      break;
+      case '-':
+         resultado = a - b;
+      break;
+      case '*':
+         resultado = a * b;
+
+   }
+}
