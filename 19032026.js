9d8cec6b7bab601fc9223c2a630da09b5c8c3384
Gustavo <gustavo.ferraz.coelho@escola.pr.gov.br>
2026-03-27 07:57:50 -0300

    update

---
+++ 19032026.js
@@ -0,0 +1,21 @@
+let numero = 30
+let decimal = 20.30
+
+let nome = "pneumoultramicroscopicossilicovulcanoconiótico"
+let nome1 = "PNEUMOULTRAMICROPICOSSILICOVULCANOCONIÓTICO"
+
+console.log(nome === nome1);
+
+const inputMinusculo = nome1.toLowerCase();
+
+console.log(nome === inputMinusculo);
+
+const senha = "minhaSenha123"
+console.log(senha.length)
+
+let nv = true;
+let nz = false;
+
+if(nv!==nz){
+    console.log(`nv é diferente, pois ele é ${nv}`)
+}
---
+++ 27032026.js
@@ -0,0 +1,12 @@
+// criar array
+const fila = [1,2,3,4,5,6];
+const filaStringAmigos = ["Gustavo","JP","Rogerio Aldegar"]
+function lerAmigos(filaStringAmigos){
+    console.log('Você tem $(filaStringAmigos.lenght} amigos')
+    for(let i = 0; i <filaStringAmigos.length;i++){
+        console.log(filaStringAmigos[1])
+        if(filaStringAmigos[i] === "JP"){
+            console.log('Index do seu melhor amigo é $(i)')
+        }
+    }
+}
